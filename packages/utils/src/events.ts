0ee1fdcbf7c62beebb2a956748dbe98e3764ae54
Jakub Freisler <jakubfreisler@phrase.com>
2021-03-14 20:19:16 +0100

    feat(utils): better typings for event targets

--- packages/utils/src/events.ts
+++ packages/utils/src/events.ts
@@ -12,23 +12,23 @@ export type CustomEventListener<E extends keyof EventDetailMap = keyof EventDeta
 // TODO: add possibility to pass event/target array?
 
 export const on: {
-  <E extends keyof EventDetailMap>(target: Element, event: E, handler: CustomEventListener<E>, options?: boolean | AddEventListenerOptions): void;
-  (target: Element, event: string, handler: EventListener, options?: boolean | AddEventListenerOptions): void;
-} = (target: Element, event: string, handler: EventListener | CustomEventListener, options?: boolean | AddEventListenerOptions) => {
+  <E extends keyof EventDetailMap>(target: EventTarget, event: E, handler: CustomEventListener<E>, options?: boolean | AddEventListenerOptions): void;
+  (target: EventTarget, event: string, handler: EventListener, options?: boolean | AddEventListenerOptions): void;
+} = (target: EventTarget, event: string, handler: EventListener | CustomEventListener, options?: boolean | AddEventListenerOptions) => {
   target.addEventListener(event, handler as EventListener, options);
 }
 
 export const off: {
-  <E extends keyof EventDetailMap>(target: Element, event: E, handler: CustomEventListener<E>): void;
-  (target: Element, event: string, handler: EventListener): void;
-} = (target: Element, event: string, handler: EventListener | CustomEventListener) => {
+  <E extends keyof EventDetailMap>(target: EventTarget, event: E, handler: CustomEventListener<E>): void;
+  (target: EventTarget, event: string, handler: EventListener): void;
+} = (target: EventTarget, event: string, handler: EventListener | CustomEventListener) => {
   target.removeEventListener(event, handler as EventListener);
 }
 
 export const dispatch: {
-  <E extends keyof EventDetailMap, P extends EventDetailMap[E]>(target: Element, event: E, ...[payload, options]: (P extends undefined ? [undefined?, EventInit?] : never)): boolean;
-  <E extends keyof EventDetailMap>(target: Element, event: E, payload: EventDetailMap[E], options?: EventInit): boolean;
+  <E extends keyof EventDetailMap, P extends EventDetailMap[E]>(target: EventTarget, event: E, ...[payload, options]: (P extends undefined ? [undefined?, EventInit?] : never)): boolean;
+  <E extends keyof EventDetailMap>(target: EventTarget, event: E, payload: EventDetailMap[E], options?: EventInit): boolean;
 } = <E extends keyof EventDetailMap>(
-  target: Element, event: E, payload?: EventDetailMap[E], options?: EventInit
+  target: EventTarget, event: E, payload?: EventDetailMap[E], options?: EventInit
 ) =>
   target.dispatchEvent(new CustomEvent(event, Object.assign({ detail: payload }, options)));
