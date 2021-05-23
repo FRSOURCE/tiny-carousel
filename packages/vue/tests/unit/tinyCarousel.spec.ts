5e4b5eee924932997fda9e68ab5f957ae489cda1
Jakub Freisler <jakubfreisler@phrase.com>
2021-05-23 15:47:05 +0200

    chore(vue): remove local @types/jest devDependency
    add TODO comment for tests

--- packages/vue/tests/unit/tinyCarousel.spec.ts
+++ packages/vue/tests/unit/tinyCarousel.spec.ts
@@ -2,6 +2,7 @@
 // import TinyCarouselComponent from '@/components/TinyCarousel.vue'
 // import { TinyCarousel } from '../../../core/dist'
 
+// TODO: tests
 it('doesn`t fail', () => {
   // const wrapper = shallowMount(TinyCarouselComponent);
   expect(1).toBe(1)
